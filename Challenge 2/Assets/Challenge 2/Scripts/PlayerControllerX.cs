03d899794f65fc88c85c85245c85e53df8abd123
frozenchicken00 <116766692+frozenchicken00@users.noreply.github.com>
2023-11-03 04:50:43 +0000

    Add Challenge

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooltime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooltime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooltime = 1;
+        }
+        if (cooltime >= 0) {
+            cooltime -= Time.deltaTime;
+        }
+    }
+}
